b93345493b24e88a985219632814fcc1a902d361
0460208 <70180650+0460208@users.noreply.github.com>
2025-01-22 12:31:11 -0500

    update

--- to do application using React copy/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-/* Component file */
-import TodoContainer from "./components/TodoContainer";
-import "./App.css";
-
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
